1d94b546a67388c080a5814631a5386f7a22bc2f
eaharonoff <emanuel.aharonoff@gmail.com>
2016-06-20 20:29:12 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,29 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student 
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    # remember each row should be a new instance of the Student class 
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL 
+      SELECT * FROM students WHERE name = ? LIMIT 1 
+      SQL
+      #Why iterate through the entire database of rows if 
+      #query is limited to one row? 
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row) #goes through each row 
+      end.first #returns first row. But how many rows does it return without .first?
   end
   
   def save
@@ -39,5 +52,50 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT() FROM students WHERE grade = 9 
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT() FROM students WHERE grade < 12
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students 
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
+  def self.first_x_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT * FROM students LIMIT #{num} 
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT 1 
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = #{grade}
+      SQL
+      DB[:conn].execute(sql)
   end
 end
